8364ee61262396fa76424e9025c2821e55708b7f
ST10119567 <st10119567@vcconnect.edu.za>
2022-09-15 12:28:49 +0200

    Initialisation

--- app/src/main/java/com/example/icetask3/MainActivity.java
+++ app/src/main/java/com/example/icetask3/MainActivity.java
@@ -78,6 +78,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             txtLocation.setText(address);
 
 
+
+
         }catch (Exception e){
             e.printStackTrace();
         }
